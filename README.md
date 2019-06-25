6ce3c6aebf49816f91198f878216f75e39ba1b17
wu.yushu <wuyushuwys@outloo.com>
2019-06-25 11:41:21 -0400

    depth & radar hold refresh in specified seconds

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 - Run with python demo.py
 - hold: whether keep previous plot in one plot(True/False) 
 
+--- 
+- Plot Depth data
+
 ## Visualization.py
 - Visualize radar message in .pcd and Depth message in .csv
 - Has been deprecated.
--- demo.py
+++ demo.py
@@ -3,20 +3,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import sensor_msgs.point_cloud2 as pc2
+#import sensor_msgs.Image as Image
 from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import Image
 import argparse
-
+from time import time
+import cv2
+from cv_bridge import CvBridge
 
 class Radar:
-    def __init__(self, scan_topic='/mmWaveDataHdl/RScan'):
+    def __init__(self, scan_topic='/mmWaveDataHdl/RScan', depth_topic='/camera/depth/image'):
         rospy.init_node('listener')
         self.scan_topic = scan_topic
+        self.depth_topic = depth_topic
         rospy.Subscriber(self.scan_topic, PointCloud2, self.scan)
-
+        rospy.Subscriber(self.depth_topic, Image, self.imaging)
         self.x = []
         self.y = []
         self.z = []
         self.intensity = []
+        self.image = []
+        self.bridge = CvBridge()
+
+    def imaging(self, depth):
+        image = self.bridge.imgmsg_to_cv2(depth)
+        cv2.imshow("Imshow", image)
+        cv2.waitKey(3)
+        self.image = np.nan_to_num(image)
 
     def scan(self, cloud):
         xyz_generator = pc2.read_points_list(cloud, skip_nans=True, field_names=("x", "y", "z", "intensity"))
@@ -29,34 +42,51 @@ class Radar:
             y.append(xyz.y)
             z.append(xyz.z)
             intensity.append(xyz.intensity)
-        self.x, self.y, self.z, self.intensity = x, y, z, intensity
+        self.x = x
+        self.y = y
+        self.z = z
+        self.intensity = intensity
 
-    def run(self, hold=True):
-        fig = plt.figure(figsize=(20,20))
+    def run(self, hold, hold_time, sampling):
+        fig = plt.figure(1, figsize=(10, 10))
         ax = plt.axes(projection='3d')
-        r = rospy.Rate(10)
-
+        r = rospy.Rate(100)
+        time_base = time()
+        ax.view_init(-90, 0)
+        data = [[], [], [], []]
         while not rospy.is_shutdown():
-            if not hold:
+            if (not hold) or (time()-time_base > hold_time):
                 ax.clear()
+                time_base = time()
+                if sampling:
+                    np.save('radar_xyzi', np.nan_to_num(np.array(data)))
+                    np.save('depth', np.array(self.image))
+                    cv2.destroyAllWindows()
+                    print('Finish Sampling')
+                    break
             ax.set_xlim3d(0, 2.5)
             ax.set_xlabel('x')
             ax.set_ylim3d(-5, 5)
             ax.set_ylabel('y')
             ax.set_zlim3d(-5, 5)
-            # ax.set_zlabel('z')
             ax.scatter(self.x, self.y, self.z)
+            data[0] += self.x
+            data[1] += self.y
+            data[2] += self.z
+            data[3] += self.intensity
             fig.canvas.draw()
-            plt.pause(0.1)
-            # r.sleep()
+            plt.pause(0.01)
+
 
 
 if __name__ == "__main__":
     parse = argparse.ArgumentParser(description="mmWave Point Cloud Visualizer",
                                     usage='use "%(prog)s --help" for more information',
                                     formatter_class=argparse.RawTextHelpFormatter)
-    # parse.add_argument('File', help="max running time")
-    parse.add_argument('--hold', type=str, default='False', help='Hold plot or not')
+    parse.add_argument('--refresh', type=float, default=2.0, help="max running time")
+    parse.add_argument('--hold', type=bool, default=False, help='Hold plot or not')
+    parse.add_argument('--sampling', type=bool, default=False, help='Sampling')
     args = parse.parse_args()
     mmwave = Radar()
-    mmwave.run(hold=False)
+    mmwave.run(hold=args.hold, hold_time=args.refresh, sampling=args.sampling)
+    rospy.spin()
